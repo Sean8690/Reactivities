5c9c15d908758a73aadad306384abc95520dba39
Sean Elias <sean.s.elias@outlook.com>
2021-03-29 20:49:43 +1100

    added Semantic UI and fix Cors issue

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            // services.AddCors(opt => {
+            //     opt.AddPolicy("CorsPolicy", policy => {
+            //         policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000/");
+            //     });
+            // });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +59,9 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //app.UseCors("CorsPolicy");
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
