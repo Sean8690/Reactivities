a947e84bbf61e27aa04c8f41fd634b827191493e
Sean Elias <sean.s.elias@outlook.com>
2022-01-26 13:07:54 +1100

    EF relationships

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
@@ -30,6 +28,7 @@ namespace API.Controllers
             return HandleResults(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
@@ -37,10 +36,17 @@ namespace API.Controllers
             return HandleResults(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
             return HandleResults(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResults(await Mediator.Send(new UpdateAttendance.Command{Id = id}));
         }
     }
 }
