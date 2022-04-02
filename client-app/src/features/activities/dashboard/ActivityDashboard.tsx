52455261571f016cef5988463689acb32e2b65fb
Sean Elias <sean.s.elias@outlook.com>
2022-04-02 21:37:21 +1100

    Added sorting and filtering

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,30 +1,57 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
-import { Grid} from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import {Button, Grid} from 'semantic-ui-react';
 import LoadingComponenet from '../../../app/layout/loadingComponent';
+import { PagingParams } from '../../../app/layout/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
-    const {loadingActivities, activityRegistry} = activityStore;
+    const {loadingActivities, activityRegistry, setPagingParams, pagination} = activityStore;
+    const [loadingNext, setLoadingNext] = useState(false);
+    
+    function handleGetNext() {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
+        //No need to pass params because it's
+        //getting it from store itself
+        loadingActivities().then(() => setLoadingNext(false));
+    }
 
     useEffect(() => {
       if (activityRegistry.size <= 1) loadingActivities();
     }, [activityRegistry.size, loadingActivities]) 
   
-  
-    if(activityStore.loadingInitial) return <LoadingComponenet content='Loading activities...' />
 
     return (
         <Grid>
             <Grid.Column width="10">
-                <ActivityList />
+            {activityStore.loadingInitial && !loadingNext ? (
+                <>
+                    <ActivityListItemPlaceholder />
+                    <ActivityListItemPlaceholder />
+                </>
+            ) : (
+                <>
+                   <ActivityList />
+                    <Button
+                            floated='right'
+                            content='More...'
+                            positive
+                            onClick={handleGetNext}
+                            loading={loadingNext}
+                            disabled={pagination?.totalPages === pagination?.currentPage}
+                        >
+                    </Button> 
+                </>
+            )}    
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters />
             </Grid.Column>
         </Grid>
-    )
+    ) 
 })
